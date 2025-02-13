2127c6107839ace1b1ac54b46a4f44ec8bb53c00
Amrit-raj-sharma <amrit@fortaxe.com>
2025-02-13 18:25:55 +0530

    Dev - made carausel dynamic

--- src/clientComponents/HeroCarousel.jsx
+++ src/clientComponents/HeroCarousel.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const HeroCarousel = () => {
+const HeroCarousel = ({ carouselData }) => {
 
-    const carouselData = [
-        {
-            id: 1,
-            image: 'carousel-bg1.png',
-            title: 'Your Trusted Partner in Precision Lab Equipment.',
-            exploreUrl: '/'
-        },
-        {
-            id: 2,
-            image: 'carousel-bg1.png',
-            title: 'Your Trusted Partner in Precision Lab Equipment.',
-            exploreUrl: '/'
-        },
-        {
-            id: 3,
-            image: 'carousel-bg1.png',
-            title: 'Your Trusted Partner in Precision Lab Equipment.',
-            exploreUrl: '/'
-        }
-    ]
+    // const carouselData = [
+    //     {
+    //         id: 1,
+    //         image: 'carousel-bg1.png',
+    //         title: 'Your Trusted Partner in Precision Lab Equipment.',
+    //         exploreUrl: '/'
+    //     },
+    //     {
+    //         id: 2,
+    //         image: 'carousel-bg1.png',
+    //         title: 'Your Trusted Partner in Precision Lab Equipment.',
+    //         exploreUrl: '/'
+    //     },
+    //     {
+    //         id: 3,
+    //         image: 'carousel-bg1.png',
+    //         title: 'Your Trusted Partner in Precision Lab Equipment.',
+    //         exploreUrl: '/'
+    //     }
+    // ]
 
   return (
     <div className="w-full h-[533.33px]">
@@ -35,10 +35,10 @@ const HeroCarousel = () => {
         showStatus={false}
         interval={3000}
       >
-        {carouselData.map((item, index) => (
-            <div  key={index} 
+        {carouselData.length > 0 && carouselData.map((item) => (
+            <div  key={item._id} 
             className="relative w-full h-[533.33px] bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: `url(${item.image})` }}>
+            style={{ backgroundImage: `url(${item.bannerImage})` }}>
             <div className="absolute inset-0 bg-black bg-opacity-40 flex flex-row xl:justify-end justify-center items-center p-8 md:p-16">
               <div className="flex flex-col items-start max-w-[617px]">
               <h2 className="text-white md:text-[49.33px] text-lg text-left md:text-4xl font-bold xl:mb-[38.08px] mb-[11px]">
