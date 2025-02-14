fa995a248366159ba5ab4a3b104970294f0b6eec
Amrit-raj-sharma <amrit@fortaxe.com>
2025-02-14 15:43:58 +0530

    Dev - crteated Not Found component

---
+++ src/clientComponents/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='flex items-center justify-center h-full w-full'>
+            <img src='/not_found.png' alt='not found' className='w-[300px] h-[300px]' />
+        </div>
+    )
+}
+
+export default NotFound
