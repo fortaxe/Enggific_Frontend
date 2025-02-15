f700162c4d74d8bd90341bc36f0101821c9ab4aa
Amrit-raj-sharma <amrit@fortaxe.com>
2025-02-15 12:20:20 +0530

    Dev - fixed css issue

--- src/clientComponents/CompanyBanners.jsx
+++ src/clientComponents/CompanyBanners.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const CompanyBanners = () => {
+  return (
+    <div className="xl:px-[60px] px-[16px] xl:py-[70px] py-[30px]">
+      <div className="grid grid-cols-2 lg:flex lg:flex-wrap lg:justify-evenly xl:gap-[29px] gap-[16px]">
+        {[...Array(6)].map((_, index) => (
+          <div
+            key={index}
+            className="xl:w-[200px] xl:h-[90px] w-[90-61px] h-[80px]"
+            style={{
+              backgroundImage: `url(/company${index + 1}.png)`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-
-    return (
-        <div className=''>
-            <div className="flex flex-wrap justify-evenly gap-[29px] px-[60px] py-[70px]">
-                {[...Array(6)].map((_, index) => (
-                    <div
-                        key={index}
-                        className="w-[200px] h-[90px]"
-                        style={{
-                            backgroundImage: `url(/company${index+1}.png)`,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                        }}
-                    >
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-}
-
-export default CompanyBanners
+export default CompanyBanners;
