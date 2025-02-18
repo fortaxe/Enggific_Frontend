0bbbb5c7e76fbddd011882306c3270ababe4a959
Amrit-raj-sharma <amrit@fortaxe.com>
2025-02-18 14:09:30 +0530

    Dev - set orange shade in bottom

--- src/clientComponents/OurCards.jsx
+++ src/clientComponents/OurCards.jsx
@@ -16,12 +16,13 @@ const OurCards = () => {
                         </div>
                     </div>
                     {/* curve-bg */}
-                    <div className='absolute bottom-0 left-0 -z-[1]'>
-                        <svg className='w-full h-[350px] xl:w-[450px] xl:h-[450px]' viewBox="0 0 450 198" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <div className='absolute bottom-0 left-0 w-full -z-[1]'>
+                        <svg className='w-full h-[200px]' viewBox="0 0 450 198" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M275.186 96.0202C165.953 16.8358 46.2378 -0.960684 0.0344238 0.0391187L0.0344342 198H450V131.513C379.661 149.909 304.149 115.516 275.186 96.0202Z" fill="#FDF3E7" />
                         </svg>
                     </div>
                 </div>
+
 
                 <div className='relative w-full sm:w-[calc(50%-14.5px)] md:w-[calc(50%-14.5px)] h-[320px] py-[61px] pl-[50px] pr-[18px] shadow-md z-[1]'>
                     <div className='z-[1]'>
@@ -36,12 +37,13 @@ const OurCards = () => {
                         </div>
                     </div>
                     {/* curve-bg */}
-                    <div className='absolute bottom-0 left-0 -z-[1]'>
-                        <svg className='w-full h-[300px] xl:w-[450px] xl:h-[450px]' viewBox="0 0 450 196" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <div className='absolute bottom-0 left-0 w-full -z-[1]'>
+                        <svg className='w-full h-[200px]' viewBox="0 0 450 196" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M237.931 103.238C170.225 140.344 51.4335 81.9139 0 48V196H450V0C401.491 5.98481 322.564 56.8557 237.931 103.238Z" fill="#FDF3E7" />
                         </svg>
                     </div>
                 </div>
+
 
             </div>
         </div>
