c81740c35ac4efc06a07aef03d06ff423fbf712f
Amrit-raj-sharma <amrit@fortaxe.com>
2025-02-19 10:14:35 +0530

    Dev - added hover background color

--- src/clientComponents/EquipmentCard.jsx
+++ src/clientComponents/EquipmentCard.jsx
@@ -25,7 +25,7 @@ const EquipmentCard = ({ equipment, page, subId }) => {
         <div onClick={handleNavigate} className='w-full sm:w-[calc(50%-14.5px)] md:w-[calc(50%-14.5px)] lg:w-[calc(33.333%-19.33px)] h-[282.95px] shadow-md cursor-pointer'>
             <div className='h-[214.42px] bg-cover bg-center border-b-[3.92px] border-[#E5810C]' style={{ backgroundImage: `url(${backgroundImage})` }}>
             </div>
-            <div className='h-[68.53px] border border-[#E0E0E0] px-[31px] flex gap-[13.2px] justify-start items-center overflow-hidden'>
+            <div className='h-[68.53px] border border-[#E0E0E0] px-[31px] flex gap-[13.2px] justify-start items-center overflow-hidden hover:border-[#E5810C] hover:bg-[#E5810C] hover:text-white transition-all duration-300'>
                 <div className=''>
                     {/* {equipment.icon} */}
                 </div>
