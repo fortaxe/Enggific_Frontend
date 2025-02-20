ad63ecae25a4f3307a4616af1951c3ac694bbc99
Amrit-raj-sharma <amrit@fortaxe.com>
2025-02-20 18:55:05 +0530

    Dev - created whatsappicon component

---
+++ src/clientComponents/WhatsAppIcon.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { MessageCircle } from 'lucide-react';
+import { BASE_URL } from '@/constants';
+import useFetchData from './utils/useFetchData';
+
+const WhatsAppIcon = () => {
+
+    const apiUrl = `${BASE_URL}/user/get/socialMediaLinks`;
+
+    const { data, loading, error } = useFetchData(apiUrl);
+
+    console.log("data www", data)
+
+    const phoneNumber = data?.links[0].whatsappNumber ? data?.links[0].whatsappNumber :'1234567890'; 
+    const message = 'Hello! I would like to chat with you.';
+
+    const handleClick = () => {
+        const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+        window.open(url, '_blank');
+    };
+
+    return (
+        <div className="fixed bottom-6 right-6 z-50">
+            <button 
+                onClick={handleClick} 
+                className="bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-all"
+                aria-label="Chat on WhatsApp"
+            >
+                <MessageCircle size={24} />
+            </button>
+        </div>
+    );
+};
+
+export default WhatsAppIcon;
