baa6d97ddfadbebb08dd8b43108b246bfa3c1cda
Amrit-raj-sharma <amrit@fortaxe.com>
2025-02-27 20:55:58 +0530

    Dev - updated

--- src/clientComponents/KnowUs.jsx
+++ src/clientComponents/KnowUs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Carousel } from 'react-responsive-carousel'
 
+
 const KnowUs = () => {
     return (
         <div className='bg-[#F8F8F8] px-[16px] pt-[94px] pb-[104px]'>
