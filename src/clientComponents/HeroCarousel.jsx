5fbb70d9a6faff6878afa0a6b95b811896ce72db
Amrit-raj-sharma <amrit@fortaxe.com>
2025-02-27 20:48:54 +0530

    Dev - updated carausel

--- src/clientComponents/HeroCarousel.jsx
+++ src/clientComponents/HeroCarousel.jsx
@@ -1,53 +1,70 @@
 import React from "react";
-import { Carousel } from "react-carousel-minimal";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useNavigate } from "react-router-dom";
+import InteractiveGrid from "./InteractiveGrid";
 
 const HeroCarousel = ({ carouselData }) => {
+
   const navigate = useNavigate();
 
-  console.log("carar", carouselData)
+    // const carouselData = [
+    //     {
+    //         id: 1,
+    //         image: 'carousel-bg1.png',
+    //         title: 'Your Trusted Partner in Precision Lab Equipment.',
+    //         exploreUrl: '/'
+    //     },
+    //     {
+    //         id: 2,
+    //         image: 'carousel-bg1.png',
+    //         title: 'Your Trusted Partner in Precision Lab Equipment.',
+    //         exploreUrl: '/'
+    //     },
+    //     {
+    //         id: 3,
+    //         image: 'carousel-bg1.png',
+    //         title: 'Your Trusted Partner in Precision Lab Equipment.',
+    //         exploreUrl: '/'
+    //     }
+    // ]
 
-  const images = ['Mesuring_Device_banner.webp', 'Control_equipment_banner.webp', 'safety_equipments_banner.webp']
-
-  const data = images.map((item) => ({
-    image: `/${item}`,
-    caption: '',
-  }));
-
-  const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 'bold',
-    color: 'white',
-    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.8)'
-  };
-
-  const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 'bold'
-  };
+    const images = ['Mesuring_Device_banner.webp', 'Control_equipment_banner.webp', 'safety_equipments_banner.webp']
 
   return (
-    <div className="w-full md:h-[533.33px] h-[206px] md:mt-[110px]">
+    <div>
+
+<div className="w-full md:h-[533.33px] h-[206px] md:mt-[110px] mt-0">
       <Carousel
-        data={data}
-        time={3000}
-        width="100%"
-        height="533.33px"
-        // captionStyle={captionStyle}
-        radius="10px"
-        slideNumber={false}
-        slideNumberStyle={slideNumberStyle}
-        automatic={true}
-        dots={true}
-        pauseIconColor="white"
-        pauseIconSize="40px"
-        slideBackgroundColor="white"
-        slideImageFit="cover"
-        thumbnails={false}
-        showNav={false}
-        onClick={(index) => navigate(carouselData[index]?.exploreUrl || '/')}
-      />
+        showArrows={true}
+        autoPlay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        interval={3000}
+      >
+        {images.length > 0 && images.map((item,index) => (
+            <div  key={index} 
+            className="relative w-full md:h-[533.33px] h-[206px] bg-cover bg-center bg-no-repeat"
+            style={{ backgroundImage: `url(${item})` }}>
+            {/* <div className="absolute inset-0 bg-black bg-opacity-40 flex flex-row xl:justify-end justify-center items-center p-8 md:p-16">
+              <div className="flex flex-col items-start max-w-[617px]">
+              <h2 className="text-white md:text-[49.33px] text-lg text-left md:text-4xl font-bold xl:mb-[38.08px] mb-[11px]">
+                {item.title}
+              </h2>
+              <button onClick={() => navigate('/product-categories')} className="xl:w-[218.67px] xl:h-[48px] w-[98px] h-[32px] flex justify-center items-center bg-orange-500 text-white xl:text-base text-xs hover:bg-orange-600 transition">
+                Explore now
+              </button>
+              </div>
+            </div> */}
+          </div>
+        )) }
+      </Carousel>
     </div>
+
+    
+    </div>
+    
   );
 };
 
