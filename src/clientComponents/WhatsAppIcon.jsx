19e8da190dc62a46b0ccc69846323b08868a5f94
Amrit-raj-sharma <amrit@fortaxe.com>
2025-02-28 16:17:09 +0530

    Dev -added animation

--- src/clientComponents/WhatsAppIcon.jsx
+++ src/clientComponents/WhatsAppIcon.jsx
@@ -20,7 +20,7 @@ const WhatsAppIcon = () => {
     };
 
     return (
-        <div className="fixed bottom-6 right-6 z-50">
+        <div className="fixed bottom-6 right-6 z-50 hover:animate-pulse animate-bounce">
             <button 
                 onClick={handleClick} 
                 className="text-white rounded-full shadow-lg transition-all"
