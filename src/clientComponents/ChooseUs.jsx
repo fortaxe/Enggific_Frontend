09830769e39d2c30694acea599fc97115e5c40c6
Amrit-raj-sharma <amrit@fortaxe.com>
2025-02-28 15:22:58 +0530

    Dev - changed text size for mobile view

--- src/clientComponents/ChooseUs.jsx
+++ src/clientComponents/ChooseUs.jsx
@@ -65,7 +65,7 @@ const ChooseUs = () => {
                            <div className="top-0 left-[5%] bg-[orange] w-[39.67px] h-[39.67px] md:w-[83px] md:h-[99px] flex items-center justify-center mb-[34px]">
                                {item.icon}
                            </div>
-                           <p className="md:text-base lg:text-[22px] text-[9px] leading-[17.61px] md:leading-normal text-textBlack font-bold break-words">
+                           <p className="text-base lg:text-[22px] text-[9px] leading-[17.61px] md:leading-normal text-textBlack font-bold break-words">
                                {item.title}
                            </p>
                            <p className="md:text-sm lg:text-base text-textBlack">{item.decription}</p>
