0577016fc71dd181239099027eaf30d0a941b2d8
amarnath666 <amarnathdhumal2001@gmail.com>
2025-03-03 12:34:18 +0530

    updated sub category page

--- src/clientComponents/CategoryCarousel.jsx
+++ src/clientComponents/CategoryCarousel.jsx
@@ -81,7 +81,7 @@ const CategoryCarousel = ({ categories, page, subName, subId }) => {
               />
             </div>
             <div className='h-[68.53px] w-full border border-[#E0E0E0] px-3 py-[20px] flex justify-center items-center border-t-gradient-to-r from-[#F8710C] to-[#F22B06] border-t-2 hover:bg-gradient-to-r from-[#F8710C] to-[#F22B06] hover:text-white transition-all duration-300'>
-              <p className='break-words text-center text-base truncate w-full capitalize'>{category.name.toLowerCase()}</p>
+              <p className='break-words text-center text-[12px] sm:text-base truncate w-full capitalize'>{category.name.toLowerCase()}</p>
             </div>
           </div>
         ))}
--- src/clientComponents/EquipmentCard.jsx
+++ src/clientComponents/EquipmentCard.jsx
@@ -4,54 +4,49 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom'
 
-const EquipmentCard = ({ equipment, page, subName , subId }) => {
-
+const EquipmentCard = ({ equipment, page, subName, subId }) => {
     const [idType, setIdType] = useState("");
-  const [id, setId] = useState("");
-
-    const navigate = useNavigate()
-
+    const [id, setId] = useState("");
+    
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     
-
     const category = {
         id: '1'
-    }
-
+    };
+    
     const handleAdd = (newId) => {
-            dispatch(addId({ idType:"category", id: newId }));
-            // setIdType("");
-            // setId("");
-            navigate(`/${(equipment.name).replace(/\s+/g, '-')}/sub-categories`)
-        
-      };
-
+        dispatch(addId({ idType: "category", id: newId }));
+        // setIdType("");
+        // setId("");
+        navigate(`/${(equipment.name).replace(/\s+/g, '-')}/sub-categories`);
+    };
+    
     const handleNavigate = () => {
-        if(page === 'sub-category'){
-            dispatch(addId({ idType:"sub-category", id: subId }));
-            navigate(`/${subName.replace(/\s+/g, '-')}/${(equipment.name).replace(/\s+/g, '-')}/products`)
+        if (page === 'sub-category') {
+            dispatch(addId({ idType: "sub-category", id: subId }));
+            navigate(`/${subName.replace(/\s+/g, '-')}/${(equipment.name).replace(/\s+/g, '-')}/products`);
         } else {
             // console.log("equipment._id", equipment._id)
-            handleAdd(equipment._id)
+            handleAdd(equipment._id);
         }
-    }
-
+    };
+    
     // console.log("page", page)
-
-    const backgroundImage = page === 'sub-category' ? equipment.productTypeLogo : equipment.categoryLogo
+    const backgroundImage = page === 'sub-category' ? equipment.productTypeLogo : equipment.categoryLogo;
     
     return (
-        <div onClick={handleNavigate} className='w-full md:w-[calc(50%-14.5px)] lg:w-[calc(33.33%-19.33px)]  xl:w-[calc(24%-19.33px)] max-h-[383px] shadow-md cursor-pointer'>
-            <div className='h-[283px] bg-cover bg-center border-b-[3.92px] border-[#E5810C]' style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div onClick={handleNavigate} className='w-full  shadow-md cursor-pointer rounded-[12px]'>
+            <div className='h-[160px] sm:h-[283px] rounded-t-[12px]  bg-cover bg-center border-b-[3.92px] ' style={{ backgroundImage: `url(${backgroundImage})` }}>
             </div>
-            <div className='h-[68.53px] border border-[#E0E0E0] px-[31px] py-[20px] flex gap-[13.2px] justify-start items-center overflow-hidden hover:border-[#E5810C] hover:bg-[#E5810C] hover:text-white transition-all duration-300'>
+            <div className='sm:h-[68.53px] px-2 sm:px-4 md:px-[31px] py-[10px] sm:py-[20px] flex gap-[13.2px] justify-center items-center overflow-hidden   transition-all duration-300'>
                 {/* <div className=''>
                     {equipment.icon}
                 </div> */}
-                <p className='break-words xl:text-lg text-base capitalize'>{equipment.name.toLowerCase()}</p>
+                <p className='break-words text-[12px] sm:text-base text-center capitalize truncate'>{equipment.name.toLowerCase()}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default EquipmentCard
+export default EquipmentCard;
