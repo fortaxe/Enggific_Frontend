7f78e02635400328dbcb1c981798ebcffe77b071
Amrit-raj-sharma <amrit@fortaxe.com>
2025-03-04 10:31:30 +0530

    Dev - changed loader

--- src/clientComponents/Loader.jsx
+++ src/clientComponents/Loader.jsx
@@ -3,11 +3,12 @@ import React from "react";
 const Loader = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            <img
+            {/* <img
                 src="/enggific_logo.png"
                 alt="Logo Loader"
                 className="w-[77.77px] h-[64px] animate-[pulse_0.8s_ease-in-out_infinite]"
-            />
+            /> */}
+            <span className="loader"></span>
         </div>
     );
 };
