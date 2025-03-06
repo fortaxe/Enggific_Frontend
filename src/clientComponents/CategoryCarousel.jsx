731b9675e1735f4eb64acef95473a95612774c89
Amrit-raj-sharma <amrit@fortaxe.com>
2025-03-06 12:38:29 +0530

    Dev - made changes in the text alignment

--- src/clientComponents/CategoryCarousel.jsx
+++ src/clientComponents/CategoryCarousel.jsx
@@ -129,12 +129,15 @@ const CategoryCarousel = ({ categories, page, subName, subId }) => {
                 className="w-full h-full object-cover"
               />
               <div className="absolute bottom-0 w-full h-[140px] bg-gradient-to-t from-black/80 via-black/50 to-transparent flex flex-row items-end justify-between p-4">
-                <h3 className="text-white text-[18px] font-bold leading-[20.13px] capitalize">{category.name.toLowerCase()}</h3>
+              <div className='flex items-center justify-between w-full'>
+              <h3 className="text-white text-[18px] font-bold leading-[28px] capitalize whitespace-pre-wrap min-w-[105] w-[179px]">{category.name.toLowerCase()}</h3>
                 <div className="bg-gradient-to-r from-[#F8710C] to-[#F22B06] w-[52px] h-[52px] flex items-center justify-center rounded-full mt-2">
                   <svg width={36} height={36} viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9.5625 26.4375L26.4375 9.5625M26.4375 9.5625H13.7812M26.4375 9.5625V22.2188" stroke="white" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round" />
                   </svg>
                 </div>
+              </div>
+                
               </div>
             </div>
           ))}
