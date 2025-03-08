d12e8be5b0e9edd88532a89f3f170ee13a8eccd1
Amrit-raj-sharma <amrit@fortaxe.com>
2025-03-08 09:13:08 +0530

    Dev - fixed alignment

--- src/clientComponents/LaunchedProducts.jsx
+++ src/clientComponents/LaunchedProducts.jsx
@@ -60,11 +60,11 @@ const LaunchedProducts = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-wrap justify-center gap-[8px] sm:gap-[29px] ">
+                <div className="flex flex-wrap md:justify-center justify-between gap-[16px] sm:gap-[29px] ">
                     {(data.products && data.products.length > 0) && data.products.slice(0, 4).map((item, index) => (
                         <div
                             key={index}
-                            className="w-[calc(50%-14.5px)] lg:w-[calc(25%-21.75px)] h-auto border border-[#D2D2D2] p-[14px] overflow-hidden rounded-[12px]"
+                            className="w-[calc(50%-8px)] lg:w-[calc(25%-21.75px)] h-auto border border-[#D2D2D2] p-[14px] overflow-hidden rounded-[12px]"
                         >
                             <div className='relative md:h-[273px] h-[159.55px]  mb-[25px] cursor-pointer' onClick={() => handleNavigate(item?.productType?.category?.name, item?.productType?.name, item?.name, item?._id)}>
                                 <div className='absolute top-[13px] left-0 md:w-[55px] md:h-[27px] w-[31px] h-[14px] bg-[#FF1C1C] flex items-center justify-center '>
